48cbff5805d7e40b0e553ea5daca8b09704899fe
Ankit Patel <kumankit098@gmail.com>
2026-07-19 22:19:25 +0530

    form fix, scroll fix. image chang

--- app.js
+++ app.js
@@ -5,11 +5,10 @@ const initApp = () => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: 'vertical',
-      gestureDirection: 'vertical',
-      smooth: true,
-      mouseMultiplier: 1.0,
-      smoothTouch: false,
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1.0,
       touchMultiplier: 2.0,
       infinite: false,
     });
